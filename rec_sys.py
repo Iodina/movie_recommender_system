bffc8cfb7df6a8518dc1e4143a3314efd1d29534
mishashamen <blogzdorov@gmail.com>
2016-05-25 05:26:39 +0300

    Filter for data file

--- rec_sys.py
+++ rec_sys.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import collections
 from recsys.algorithm.factorize import SVD
 import rating_matrix as rm
 import operator
 import numpy as np
+import csv
+from shutil import copyfile
 
 __author__ = 'mishashamen'
 
 class Recommender:
-    def __init__(self):
+    def __init__(self, datafile_path=None):
         self.svd = SVD()
         self.matrix = None
-        self.datafile_path = None
+        self.datafile_path = datafile_path
 
     def load_web_data(self, filename, film_names_with_rate_list, K, min_values,
                   MAX_COUNT_USER_FILMS=None, MAX_COUNT_FILM_USERS=None):
@@ -74,18 +77,54 @@ class Recommender:
         self.svd.load_data(self.datafile_path, sep=' ', format={'col': 1, 'row': 0, 'value': 2, 'ids': int})
         self.svd.compute(K, min_values, pre_normalize, mean_center, post_normalize, savefile=None)
 
+    def filter_films_data(self, min_user_votes):
+        film_indexes = []
+        counter = collections.Counter()
+        with open(self.datafile_path, 'rb') as my_file:
+            r = csv.reader(my_file)
+            for row in r:
+                user_index, film_index, rate = row[0].split(' ')
+                counter[int(film_index)] += 1
+
+            for k, v in counter.iteritems():
+                if v < min_user_votes:
+                    film_indexes.append(k)
+
+        copyfile(self.datafile_path+'_user_map', self.datafile_path+'_'+str(min_user_votes)+'_user_map')
+
+        new_indexes = {}
+        with open(self.datafile_path+'_film_map', 'rb') as read_file:
+            r = csv.reader(read_file)
+            with open(self.datafile_path+'_'+str(min_user_votes)+'_film_map', 'wb') as write_file:
+                wr = csv.writer(write_file, delimiter=' ')
+                index = 0
+                for row in r:
+                    film_index, film_id = row[0].split(' ')
+                    if int(film_index) in film_indexes:
+                        continue
+                    new_indexes[film_index] = index
+                    wr.writerow([index, film_id])
+                    index += 1
+
+        with open(self.datafile_path, 'rb') as read_file:
+            r = csv.reader(read_file)
+            with open(self.datafile_path+'_'+str(min_user_votes), 'wb') as write_file:
+                wr = csv.writer(write_file, delimiter=' ')
+                for row in r:
+                    user_index, film_index, rate = row[0].split(' ')
+                    if int(film_index) in film_indexes:
+                        continue
+                    wr.writerow([user_index, new_indexes[film_index], rate])
+
 
 if __name__ == "__main__":
-    recommender = Recommender()
-    # recommender.load_web_data('dataset',
-    #                           [{'Запах женщины': 9, 'The Usual Suspects': 8, 'The Departed': 8,
-    #                             'Тутси': 7, 'Выпускник': 10, 'Залечь на дно в Брюгге': 4, 'Евротур': 7,
-    #                             'Goodfellas': 6, 'Донни Браско': 8, 'Амели': 3, 'Идиократия': 7}],
-    #                           100, 0, 10, 10)
-    recommender.load_local_data('dataset', K=100, min_values=0)
-    m = recommender.matrix.get_rating_matrix()
-    print recommender.matrix.find_films_to_filter(2)
-    print m.shape
-    recommender.matrix.filter_films(2)
+    r = Recommender('dataset')
+    # r.load_local_data('dataset', 100, 0)
+    # print len(r.matrix.find_films_to_filter(2))
+    # print r.matrix.rating_matrix.shape[1]
+    r.filter_films_data(15)
+    # r.load_local_data('small_dataset_2', 100, 0)
+    # print r.matrix.rating_matrix.shape[1]
+
 
 
