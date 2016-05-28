443b725436bb3af1f49d8198a02317507635b048
Mariya Metelitsa <mariya.metelitsa@gmail.com>
2016-05-28 20:34:37 +0300

    Added profiling

--- social_choice.py
+++ social_choice.py
@@ -37,18 +37,19 @@ class GroupRecommender(Recommender):
     def _norm(self, vector):
         return np.linalg.norm(vector)
 
-    def _get_top_list(self, res_vector, top=None, relative=True):
+    def _get_top_list(self, res_vector, top=None, relative=True, title=True):
         """
         Determine top `top` films recommended for a Group
         by Group Preferences vector
         :param res_vector: 1D np.array with Group Preferences
         :param top: int number of top movies to recommend
         :param relative: if scores are relative or absolute (weighted)
+        :param title: if we need title or not
         :return: [('title' str, score int), (,), (,) ...]
         recommendation for the group
         """
         if not top:
-            top = 10
+            return res_vector
         prediction = {}
         for index in xrange(len(res_vector)):
             film = self.matrix.indexes_films_map[index]
@@ -56,22 +57,26 @@ class GroupRecommender(Recommender):
 
         prediction = sorted(prediction.items(), key=operator.itemgetter(1))
         prediction = reversed(prediction[-top:])
-        titled_prediction = []
 
         if relative:
+            relative_predict = []
             k = 0
             for i in prediction:
-                titled_prediction.append((i[0].get_title(), top - k))
+                relative_predict.append((i[0], top - k))
                 k += 1
-        else:
-            for i in prediction:
-                if i[1]:
-                    titled_prediction.append((i[0].get_title(), i[1]))
-        return titled_prediction
+            prediction = relative_predict
 
-    def predict_for_group(self, aggregation='additive', **kwargs):
+        if title:
+            titled_predict = []
+            for i in prediction:
+                titled_predict.append((i[0].get_title(), i[1]))
+            prediction = titled_predict
+
+        return prediction
+
+    def predict_for_group_merging_recommendations(self, aggregation='additive', **kwargs):
         """
-        Return Group Recommendation
+        Return Group Recommendation based on merging individual recommendations
         :param aggregation: aggregation function
         :param kwargs: threshold=3, top=10, weights=[1, 3, 5] and others
         :return:
@@ -80,6 +85,16 @@ class GroupRecommender(Recommender):
         aggregate = self.aggregation_function.get(aggregation)
         return aggregate(**kwargs)
 
+    def predict_for_group_merging_profiles(self, **kwargs):
+        """
+        Return Group Recommendation based on merging profiles
+        :param kwargs:
+        :return:
+        """
+        # TODO Implement another method
+        # Need to get all fake user's preferences and
+        # combine them into one fat fake user and make prediction for him
+
     def additive(self, **kwargs):
         """Calculate Group preference with additive strategy
         :param top: int
@@ -87,7 +102,7 @@ class GroupRecommender(Recommender):
         """
         top = kwargs.get('top')
         res_vector = self.predictions.sum(axis=0)
-        return self._get_top_list(res_vector, top=top, relative=True)
+        # return self._get_top_list(res_vector, top=top, relative=True)
 
     def multiplicative(self, **kwargs):
         """Calculate Group preference with multiplicative strategy
@@ -96,7 +111,7 @@ class GroupRecommender(Recommender):
         """
         top = kwargs.get('top')
         res_vector = self.predictions.prod(axis=0)
-        return self._get_top_list(res_vector, top=top, relative=True)
+        # return self._get_top_list(res_vector, top=top, relative=True)
 
     def average(self, **kwargs):
         """
@@ -106,7 +121,7 @@ class GroupRecommender(Recommender):
         """
         top = kwargs.get('top')
         res_vector = np.average(self.predictions, axis=0)
-        return self._get_top_list(res_vector, top=top, relative=False)
+        # return self._get_top_list(res_vector, top=top, relative=False)
 
     def average_without_misery(self, **kwargs):
         """
@@ -122,7 +137,7 @@ class GroupRecommender(Recommender):
         idx = mask.any(axis=0)
         predict[:, idx] = 0
         res_vector = np.average(predict, axis=0)
-        return self._get_top_list(res_vector, top=top, relative=False)
+        # return self._get_top_list(res_vector, top=top, relative=False)
 
     def least_misery(self, **kwargs):
         """
@@ -132,7 +147,7 @@ class GroupRecommender(Recommender):
         """
         top = kwargs.get('top')
         res_vector = np.amin(self.predictions, axis=0)
-        return self._get_top_list(res_vector, top=top, relative=True)
+        # return self._get_top_list(res_vector, top=top, relative=True)
 
     def most_pleasure(self, **kwargs):
         """
@@ -142,7 +157,7 @@ class GroupRecommender(Recommender):
         """
         top = kwargs.get('top')
         res_vector = np.amax(self.predictions, axis=0)
-        return self._get_top_list(res_vector, top=top, relative=True)
+        # return self._get_top_list(res_vector, top=top, relative=True)
 
     def fairness(self, **kwargs):
         """
@@ -169,7 +184,7 @@ class GroupRecommender(Recommender):
                     k -= 1
                     if k == 0:
                         break
-        return self._get_top_list(res_vector, top=top, relative=True)
+        # return self._get_top_list(res_vector, top=top, relative=True)
 
     def plurality_voting(self, **kwargs):
         """
@@ -193,7 +208,7 @@ class GroupRecommender(Recommender):
                 k -= 1
                 if k == 0:
                     break
-        return self._get_top_list(res_vector, top=top, relative=True)
+        # return self._get_top_list(res_vector, top=top, relative=True)
 
     def borda(self, **kwargs):
         """
@@ -216,7 +231,7 @@ class GroupRecommender(Recommender):
                     self.predictions[row][j] = borda
                 k += num_of_same
         res_vector = self.predictions.sum(axis=0)
-        return self._get_top_list(res_vector, top=top, relative=True)
+        # return self._get_top_list(res_vector, top=top, relative=True)
 
     def copeland(self, **kwargs):
         """
@@ -236,16 +251,18 @@ class GroupRecommender(Recommender):
         A = np.zeros((num_of_items, num_of_items))
         for i in range(num_of_items):
             for j in range(i, num_of_items):
-                if i == j or wins(i, j) == looses(i, j):
+                w = wins(i, j)
+                l = looses(i, j)
+                if i == j or w == l:
                     A[i, j] = A[j, i] = 0
-                elif wins(i, j) > looses(i, j):
+                elif w > l:
                     A[i, j] = 1
                     A[j, i] = -1
-                elif wins(i, j) < looses(i, j):
+                else:
                     A[i, j] = -1
                     A[j, i] = 1
         res_vector = A.sum(axis=0)
-        return self._get_top_list(res_vector, top=top, relative=True)
+        # return self._get_top_list(res_vector, top=top, relative=True)
 
     def approval_voting(self, **kwargs):
         """
@@ -264,13 +281,17 @@ class GroupRecommender(Recommender):
                 if approved(i, j):
                     A[i, j] = 1
         res_vector = A.sum(axis=0)
-        return self._get_top_list(res_vector, top=top, relative=True)
+        # return self._get_top_list(res_vector, top=top, relative=True)
 
 
 if __name__ == "__main__":
+
+    import cProfile
+
     gr = GroupRecommender('test_dataset')
     gr.load_local_data('test_dataset', 100, 0)
-    result = gr.predict_for_group(aggregation='approval_voting', threshold=5, top=10, l=3)
-    for i in result:
-        print i[0], i[1], "\n"
-        # print result
+    cProfile.run("result = gr.predict_for_group_merging_recommendations(aggregation='additive', threshold=5, top=10, l=3)")
+    # result = gr.predict_for_group_merging_recommendations(aggregation='approval_voting', threshold=5, top=10, l=3)
+    # for i in result:
+    #     print i[0], i[1], "\n"
+    # print result
