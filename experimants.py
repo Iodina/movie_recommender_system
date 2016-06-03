3aab1525bf18e32fe1db3e7ebc2d46fa1ee02953
Mariya Metelitsa <mariya.metelitsa@gmail.com>
2016-06-03 09:39:36 +0300

    minor

--- experimants.py
+++ experimants.py
@@ -12,8 +12,8 @@ def save_to_xls(gr=None, title='experiment'):
 
 
     ws = wb.add_sheet('Evaluation Experiment')
-    ws.write(0, 0, 'Method', style0)
-    ws.write(1, 0, 'Merging Recommendations', style0)
+    # ws.write(0, 0, 'Method', style0)
+    # ws.write(1, 0, 'Merging Recommendations', style0)
 
 
 
@@ -25,97 +25,97 @@ def save_to_xls(gr=None, title='experiment'):
 # 'copeland',
 
     #ranking metrics
-
-    list_L = ['fairness', 'plurality_voting']
-    list_T = ['average_without_misery','approval_voting']
-    list_all = ['additive', 'multiplicative', 'average', 'borda', 'least_misery', 'most_pleasure']
-
-    l = gr.matrix.rating_matrix.shape[1]
-    threshold = 2
-
-    i = 2
-    for fun in list_all:
-        res = gr.evaluate(aggregation=fun)
-        ws.write(i, 0, '%s'%(fun,))
-        ws.write(i, 1, '%s'%(res[0],))
-        ws.write(i, 2, '%s'%(res[1],))
-        i += 1
-
-    i += 1
-    for fun in list_T:
-        ws.write(i, 0, '%s'%(fun,), style0)
-        ws.write(i+1, 0, 'T')
-        for k in range(5):
-            threshold += 1
-            ws.write(i+1, k+1, '%s'%(threshold,))
-            res = gr.evaluate(aggregation=fun, threshold=threshold)
-            ws.write(i+2, k+1, '%s'%(res[0],))
-            ws.write(i+3, k+1, '%s'%(res[1],))
-        i += 4
-        threshold = 2
-
-    i += 1
-    for fun in list_L:
-        ws.write(i, 0, '%s'%(fun,), style0)
-        ws.write(i+1, 0, 'L')
-        for k in range(5):
-            l = int(l/100 + k*50)
-            ws.write(i+1, k+1, '%s'%(l,))
-            res = gr.evaluate(aggregation=fun, l=l)
-            ws.write(i+2, k+1, '%s'%(res[0],))
-            ws.write(i+3, k+1, '%s'%(res[1],))
-        i += 4
-        l = gr.matrix.rating_matrix.shape[1]
-
-    i += 1
-
-    # this changes matrix
-
-    # ws.write(i, 0, 'Before_raking', style0)
-    # ws.write(i+1, 1, "%s value")
-    # ws.write(i+2, 2, "%s value")
     #
-    # i += 3
+    # list_L = ['fairness', 'plurality_voting']
+    # list_T = ['average_without_misery','approval_voting']
+    # list_all = ['additive', 'multiplicative', 'average', 'borda', 'least_misery', 'most_pleasure']
+    #
+    # l = gr.matrix.rating_matrix.shape[1]
+    # threshold = 2
+    #
+    # i = 2
+    # for fun in list_all:
+    #     res = gr.evaluate(aggregation=fun)
+    #     ws.write(i, 0, '%s'%(fun,))
+    #     ws.write(i, 1, '%s'%(res[0],))
+    #     ws.write(i, 2, '%s'%(res[1],))
+    #     i += 1
+    #
+    # i += 1
+    # for fun in list_T:
+    #     ws.write(i, 0, '%s'%(fun,), style0)
+    #     ws.write(i+1, 0, 'T')
+    #     for k in range(5):
+    #         threshold += 1
+    #         ws.write(i+1, k+1, '%s'%(threshold,))
+    #         res = gr.evaluate(aggregation=fun, threshold=threshold)
+    #         ws.write(i+2, k+1, '%s'%(res[0],))
+    #         ws.write(i+3, k+1, '%s'%(res[1],))
+    #     i += 4
+    #     threshold = 2
+    #
+    # i += 1
+    # for fun in list_L:
+    #     ws.write(i, 0, '%s'%(fun,), style0)
+    #     ws.write(i+1, 0, 'L')
+    #     for k in range(5):
+    #         l = int(l/100 + k*50)
+    #         ws.write(i+1, k+1, '%s'%(l,))
+    #         res = gr.evaluate(aggregation=fun, l=l)
+    #         ws.write(i+2, k+1, '%s'%(res[0],))
+    #         ws.write(i+3, k+1, '%s'%(res[1],))
+    #     i += 4
+    #     l = gr.matrix.rating_matrix.shape[1]
+    #
+    # i += 1
 
-    res = gr.evaluate_aggregation_after('average')
+    this changes matrix
 
-    ws.write(i, 0, 'Accuracy', style0)
-    ws.write(i+1, 0, 'After, average', style0)
-    ws.write(i+2, 0, 'MAE')
-    ws.write(i+3, 0, 'RMSE')
-    ws.write(i+2, 1, res[0])
-    ws.write(i+3, 1, res[1])
-    ws.write(i+2, 2, res[2])
-    ws.write(i+3, 2, res[3])
+    ws.write(0, 0, 'Before_raking', style0)
+    ws.write(1, 1, "%s value")
+    ws.write(1, 2, "%s value")
 
-    i += 5
+    
 
-    ws.write(i, 0, 'After av_without', style0)
-    ws.write(i+1, 0, 'T')
-    ws.write(i+2, 0, 'MAE')
-    ws.write(i+4, 0, 'RMSE')
-
-    for k in range(5):
-        threshold += 1
-        ws.write(i+1, k+1, '%s'%(threshold,))
-        res = gr.evaluate_aggregation_after('average', threshold)
-        ws.write(i+2, k+1, res[0])
-        ws.write(i+3, k+1, res[2])
-        ws.write(i+4, k+1, res[1])
-        ws.write(i+5, k+1, res[3])
-
-
-    threshold = 2
-
-    res = gr.evaluate_aggregation_before()
-    i += 6
-    ws.write(i, 0, 'Before', style0)
-    ws.write(i+1, 0, 'MAE')
-    ws.write(i+2, 0, 'RMSE')
-    ws.write(i+1, 1, res[0])
-    ws.write(i+2, 1, res[1])
-    ws.write(i+1, 2, res[2])
-    ws.write(i+2, 2, res[3])
+    # res = gr.evaluate_aggregation_after('average')
+    #
+    # ws.write(i, 0, 'Accuracy', style0)
+    # ws.write(i+1, 0, 'After, average', style0)
+    # ws.write(i+2, 0, 'MAE')
+    # ws.write(i+3, 0, 'RMSE')
+    # ws.write(i+2, 1, res[0])
+    # ws.write(i+3, 1, res[1])
+    # ws.write(i+2, 2, res[2])
+    # ws.write(i+3, 2, res[3])
+    #
+    # i += 5
+    #
+    # ws.write(i, 0, 'After av_without', style0)
+    # ws.write(i+1, 0, 'T')
+    # ws.write(i+2, 0, 'MAE')
+    # ws.write(i+4, 0, 'RMSE')
+    #
+    # for k in range(5):
+    #     threshold += 1
+    #     ws.write(i+1, k+1, '%s'%(threshold,))
+    #     res = gr.evaluate_aggregation_after('average', threshold)
+    #     ws.write(i+2, k+1, res[0])
+    #     ws.write(i+3, k+1, res[2])
+    #     ws.write(i+4, k+1, res[1])
+    #     ws.write(i+5, k+1, res[3])
+    #
+    #
+    # threshold = 2
+    #
+    # res = gr.evaluate_aggregation_before()
+    # i += 6
+    # ws.write(i, 0, 'Before', style0)
+    # ws.write(i+1, 0, 'MAE')
+    # ws.write(i+2, 0, 'RMSE')
+    # ws.write(i+1, 1, res[0])
+    # ws.write(i+2, 1, res[1])
+    # ws.write(i+1, 2, res[2])
+    # ws.write(i+2, 2, res[3])
 
 
     wb.save('%s.xls' % (title,))
@@ -127,13 +127,13 @@ if __name__ == "__main__":
 
 
     gr = GroupRecommender('2_users_dataset_3')
-    save_to_xls(gr=gr, title='exp/experiment_3_filtered')
+    save_to_xls(gr=gr, title='exp/experiment_3_filtered_before_ranking')
 
     gr = GroupRecommender('5_users_dataset_7')
-    save_to_xls(gr=gr, title='exp/experiment_5_filtered')
+    save_to_xls(gr=gr, title='exp/experiment_5_filtered_before_ranking')
 
     gr = GroupRecommender('8_users_dataset_15')
-    save_to_xls(gr=gr, title='exp/experiment_8_filtered')
+    save_to_xls(gr=gr, title='exp/experiment_8_filtered_before_ranking')
 
 
 
